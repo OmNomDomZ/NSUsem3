38a64248a6dddd784155b1a9c60958f64ddf4059
v.rabetskii <v.rabetskii@g.nsu.ru>
2023-09-17 15:22:43 +0700

    insert into Map

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
 
 class FlatMap
 {
@@ -6,64 +11,158 @@ public:
     // стандартный конструктор
     FlatMap()
     {
-        Element = new element;
-        Element->key = new std::string();
-        Element->value = new std::string();
+        capacity = 10;
+        Map = new element[capacity];
     }
 
     // конструктор копирования
-    FlatMap(const FlatMap& other_map)
-    {   Element = new element;
-        this->Element->key = new std::string(*other_map.Element->key);
-        this->Element->value = new std::string(*other_map.Element->value);
+    FlatMap(const FlatMap &other_map)
+    {
+        capacity = other_map.capacity;
+        Map = new element[capacity];
+        for (std::size_t i = 0; i < capacity; ++i)
+        {
+            if (other_map.Map[i].key == nullptr)
+            {
+                break;
+            }
+            Map[i].key = new string(*(other_map.Map[i].key));
+            Map[i].value = new string(*(other_map.Map[i].value));
+        }
+
     }
 
     // деструктор
     ~FlatMap()
     {
-        // В случае std::string, деструктор автоматически управляет памятью
-        // и не требует явного удаления
+        for (std::size_t i = 0; i < capacity; ++i)
+        {
+            if (Map[i].key == nullptr)
+            {
+                break;
+            }
+            delete Map[i].key;
+            delete Map[i].value;
+        }
+        delete[] Map;
     }
 
     // перегрузка оператора присваивания
-    FlatMap& operator=(const FlatMap& other_map)
+    FlatMap& operator=(const FlatMap &other_map)
     {
-        delete this->Element->key;
-        delete this->Element->value;
+        for (std::size_t i = 0; i < capacity; ++i)
+        {
+            if (Map[i].key != nullptr)
+            {
+                delete this->Map[i].key;
+                delete Map[i].value;
+            }
+            else
+            {
+                break;
+            }
+        }
+        delete[] Map;
 
-        this->Element->key = new std::string(*other_map.Element->key);
-        this->Element->value = new std::string(*other_map.Element->value);
+        capacity = other_map.capacity;
+        Map = new element[capacity];
+        for (std::size_t i = 0; i < capacity; ++i)
+        {
+            if (other_map.Map[i].key != nullptr)
+            {
+                Map[i].key = new string(*(other_map.Map[i].key));
+                Map[i].value = new string(*(other_map.Map[i].value));
+            }
+            else
+            {
+                break;
+            }
+        }
 
         return *this;
     }
 
-    // // получить количество элементов в таблице
-    // std::size_t size() const;
+    // получить количество элементов в таблице
+    std::size_t size() const
+    {
+        std::size_t count = 0;
+        for (std::size_t i = 0; i < capacity; ++i)
+        {
+            if (Map[i].key != nullptr)
+            {
+                count++;
+            }
+            return count;
+        }
+        return count;
+    }
 
-    // // доступ / вставка элемента по ключу
-    // std::string& operator[](const std::string &key);
+    // доступ / вставка элемента по ключу
+    string& operator[](const string &key)
+    {
+        std::size_t mapSize = size();
+        if (mapSize != capacity)
+        {
+            for (std::size_t i = 0; i < mapSize; ++i)
+            {
+                if (*Map[i].key == key)
+                {
+                    return (*Map[i].value);
+                }
+            }
+        }
+
+        if (mapSize == capacity)
+        {
+            capacity *= 2;
+            element* newMap = new element[capacity];
+            for (std::size_t i = 0; i < mapSize; ++i)
+            {
+                newMap[i].key = new string(*(Map[i].key));
+                newMap[i].value = new string(*(Map[i].value));
+            }
+            Map = newMap;
+            delete[] newMap;
+        }
+        Map[mapSize].key = new string(key);
+        Map[mapSize].value = new string("");
+        return *(Map[mapSize].value);
+    }
 
     // // возвращает true, если запись с таким ключом присутствует в таблице
-    // bool contains(const std::string &key);
+    // bool contains(const string &key);
 
     // // удаление элемента по ключу, возвращает количество удаленных элементов (0 или 1)
-    // std::size_t erase(const std::string &key);
+    // std::size_t erase(const string &key);
 
     // // очистка таблицы, после которой size() возвращает 0, а contains() - false на любой ключ
     // void clear();
 
-private:
+// private:
     struct element
     {
-        std::string* key;
-        std::string* value;
+        string* key;
+        string* value;
     };
 
-    element* Element;
+    element* Map;
+    std::size_t capacity;
 };
 
-
-int main()
-{
-    return 0;
-}
+// int main()
+// {
+//     FlatMap a = FlatMap();
+//     string* d = new string("klND");
+//     string* v = new string("boo");
+//     a.Map[0].key = d;
+//     a.Map[0].value = v;
+//     FlatMap b = FlatMap();
+//     string* g = new string("klkdfnD");
+//     string* h = new string("ooo");
+//     b.Map[0].key = g;
+//     b.Map[0].value = h;
+//     b = a;
+//     // string value = a["klND"];
+//     // cout << value << endl;
+//     return 0;
+// }
