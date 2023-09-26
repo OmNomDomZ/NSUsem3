b4dedbd08db12729f903cdfe23d04f794eebdf91
v.rabetskii <v.rabetskii@g.nsu.ru>
2023-09-26 17:54:43 +0700

    add ed.md and added some tests

--- lab1/test/FlatMap_test.cpp
+++ lab1/test/FlatMap_test.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <utility>
 
 std::vector<std::vector<std::string>> TestCase1
 {
@@ -13,6 +12,24 @@ std::vector<std::vector<std::string>> TestCase1
     {"key3", "value3"},
     {"key4", "value4"},
     {"key5", "value5"}
+};
+
+std::vector<std::vector<std::string>> TestCase2
+{
+    {"key5", "value5"},
+    {"key4", "value4"},
+    {"key3", "value3"},
+    {"key2", "value2"},
+    {"key1", "value1"}
+};
+
+std::vector<std::vector<std::string>> TestCase3
+{
+    {"key1", "value1"},
+    {"key5", "value5"},
+    {"key2", "value2"},
+    {"key4", "value4"},
+    {"key3", "value3"}
 };
 
 TEST(FlatMapTest, EmptyMap)
@@ -29,5 +46,86 @@ TEST(FlatMapTest, Erase1)
         map[param[0]] = param[1];
     }
 
-    EXPECT_TRUE(map.contains(TestCase1[0][0]));
+    for (const auto& param : TestCase1)
+    {
+        EXPECT_EQ(map.erase(param[0]), 1);
+        EXPECT_FALSE(map.contains(param[0]));
+    }
+    EXPECT_EQ(map.size(), 0);
 }
+
+TEST(FlatMapTest, Erase2)
+{
+    FlatMap map;
+    for (const auto& param : TestCase2)
+    {
+        map[param[0]] = param[1];
+    }
+
+    for (const auto& param : TestCase2)
+    {
+        EXPECT_EQ(map.erase(param[0]), 1);
+        EXPECT_FALSE(map.contains(param[0]));
+    }
+    EXPECT_EQ(map.size(), 0);
+}
+
+TEST(FlatMapTest, Erase3)
+{
+    FlatMap map;
+    for (const auto& param : TestCase3)
+    {
+        map[param[0]] = param[1];
+    }
+
+    for (const auto& param : TestCase3)
+    {
+        EXPECT_EQ(map.erase(param[0]), 1);
+        EXPECT_FALSE(map.contains(param[0]));
+    }
+    EXPECT_EQ(map.size(), 0);
+}
+
+TEST(FlatMapTest, Insert1)
+{
+    FlatMap map;
+    for (const auto& param : TestCase1)
+    {
+        map[param[0]] = param[1];
+    }
+
+    for (const auto& param : TestCase1)
+    {
+        EXPECT_EQ(map[param[0]], param[1]);
+    }
+}
+
+TEST(FlatMapTest, Insert2)
+{
+    FlatMap map;
+    for (const auto& param : TestCase2)
+    {
+        map[param[0]] = param[1];
+    }
+
+    for (const auto& param : TestCase2)
+    {
+        EXPECT_EQ(map[param[0]], param[1]);
+    }
+}
+
+TEST(FlatMapTest, Insert3)
+{
+    FlatMap map;
+    for (const auto& param : TestCase3)
+    {
+        map[param[0]] = param[1];
+    }
+
+    for (const auto& param : TestCase3)
+    {
+        EXPECT_EQ(map[param[0]], param[1]);
+    }
+}
+
+    
